a6cef2a4d7c2d49127b19a6a94ab71fb3ed5cfa6
code-tzh <75636417+code-tzh@users.noreply.github.com>
2021-03-23 13:22:58 +0300

    Update UserServiceImpl.java

--- src/main/java/web/services/UserServiceImpl.java
+++ src/main/java/web/services/UserServiceImpl.java
@@ -4,11 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import web.dao.UserDao;
-import web.model.Role;
 import web.model.User;
-
 import java.util.List;
-import java.util.Set;
 
 @Service
 @Transactional
